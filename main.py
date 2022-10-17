7705be27c017e6d55a9f98700aa9a13adc57940e
MajaLu <majaglibo@gmail.com>
2022-10-17 19:13:08 +0200

    databse fix

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ import os
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///test.db")
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+# app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///test.db")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", 'sqlite:///' + os.path.join(basedir, 'todos.db'))
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
 
