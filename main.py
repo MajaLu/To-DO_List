1b746cd0f94ab3c6c992b0c3362c1df5c5b87e40
MajaLu <majaglibo@gmail.com>
2022-10-17 19:53:03 +0200

    port 2

--- main.py
+++ main.py
@@ -73,4 +73,5 @@ def delete_task(id):
 
 
 if __name__ == "__main__":
-    app.run(host="localhost", port=8000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
